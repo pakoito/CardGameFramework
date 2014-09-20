198fdded3b292b8e390253933255799b8dbc4b4f
pakoito <pakoitogod@gmail.com>
2014-09-20 20:12:51 +0100

    code reformat

--- cgfw-core/src/main/java/com/pacoworks/cardframework/systems/BasePhaseSystem.java
+++ cgfw-core/src/main/java/com/pacoworks/cardframework/systems/BasePhaseSystem.java
@@ -1,3 +1,4 @@
+
 package com.pacoworks.cardframework.systems;
 
 import com.artemis.Aspect;
@@ -6,10 +7,9 @@ import com.artemis.EntitySystem;
 /**
  * Created by Paco on 20/09/2014.
  */
-public abstract class BasePhaseSystem extends EntitySystem implements IPhaseSystem{
+public abstract class BasePhaseSystem extends EntitySystem implements IPhaseSystem {
     /**
-     * Creates an entity system that uses the specified aspect as a matcher
-     * against entities.
+     * Creates an entity system that uses the specified aspect as a matcher against entities.
      *
      * @param aspect to match against entities
      */
