a7afa0ac265a26a2ba14eca075cd556209cbb2d3
pakoito <pakoitogod@gmail.com>
2014-11-16 23:33:42 +0000

    fixed bogus dependency

--- cgfw-core/src/main/java/com/pacoworks/cardframework/systems/IPhaseSystem.java
+++ cgfw-core/src/main/java/com/pacoworks/cardframework/systems/IPhaseSystem.java
@@ -5,5 +5,5 @@ package com.pacoworks.cardframework.systems;
  * Created by Paco on 20/09/2014. See LICENSE.md
  */
 public interface IPhaseSystem {
-    public BasePhaseSystem[] pushSystems();
+    public IPhaseSystem[] pushSystems();
 }
